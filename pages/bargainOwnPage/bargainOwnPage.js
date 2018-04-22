5fa4fe7d27dea663ab8a05d1f76352730acdfba9
dearW <1029219450@qq.com>
2018-04-22 13:38:54 +0800

    砍价

--- pages/bargainOwnPage/bargainOwnPage.js
+++ pages/bargainOwnPage/bargainOwnPage.js
@@ -1,9 +1,13 @@
-Page({
+var app = getApp();
+var appData = app.globalData;
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
+    showModal: true,
+    closeIcon: '../../images/icon/close.png',
     barginOwnData: {
       userPortrait: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1524208554&di=d9b6ddb674b126952257281bc081d6ea&imgtype=jpg&er=1&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2Fe1fe9925bc315c602050233b87b1cb1348547718.jpg',
       userName: '丽丽',
@@ -29,4 +33,31 @@ Page({
       orderId:2
     }
   },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 请求数据
+    this.loadData(options.id);
+  },
+  loadData: function (id) {
+    var self = this;
+    console.log(id);
+    appData.Tool.getBargainInfo().then(function (result) {
+
+    }).catch(function (error) {
+        console.log(error);
+        wx.hideLoading()
+
+    });
+  },
+  showModalBtn: function () {
+    this.setData({ showModal: true });
+  },
+  hideModal: function () {
+    this.setData({ showModal: false });
+  },
+  shareBtn: function () {
+    // 分享
+  }
 })
