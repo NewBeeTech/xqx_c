fda487bf754f1b043e009c0446a34ca92ae1a315
dearW <1029219450@qq.com>
2018-04-23 21:43:08 +0800

    成功页面

--- pages/bargainSuccessPage/bargainSuccessPage.js
+++ pages/bargainSuccessPage/bargainSuccessPage.js
@@ -1,17 +1,24 @@
+var app = getApp();
+var appData = app.globalData;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    pageInfo: {
+      money: 0,
+      ratio: 0,
+      name: '',
+      join_time: ''
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+     this.getInfo()
   },
 
   /**
@@ -27,39 +34,32 @@ Page({
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  getInfo: function () {
+    // 获取支付成功之后的详情
+    const obj = {
+      token: wx.getStorageSync('token'),
+      cnd: '327' // 订单ID
+    }
+    const that = this;
+    appData.Tool.shareBargain(obj).then(function (res) {
+      wx.hideLoading();
+      if (res.code === 0) {
+         that.setData({ pageInfo: res.data })
+      } else {
+        wx.showToast({
+            title: res.message,
+            icon: 'none',
+            duration: 2000
+        })
+      }
+    }).catch(function (err) {
+        wx.hideLoading();
+        console.log(err)
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  goToGoodInfo: function () {
+    wx.navigateTo({
+        url: ''
+    })
   }
 })
