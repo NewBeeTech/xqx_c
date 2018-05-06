3a55f24cb65f988f38a455dfbf542545180929c9
huliyou <liyoupie@gmail.com>
2018-05-06 16:30:06 +0800

    去除之前别人写的toast

--- pages/PaymentSuccess/PaymentSuccess.js
+++ pages/PaymentSuccess/PaymentSuccess.js
@@ -31,13 +31,15 @@ Page({
       self.setData({
         info:res.data
       });
+      const info = res.data;
       self.info.curR = ~~(info.real_charge * 100 * info.cur_ratio / 100) ? ~~(info.real_charge * 100 * info.cur_ratio / 100) / 100 : 0.01;
     }).catch(function(err){
+      console.warn(err);
       wx.hideLoading();
-      wx.showToast({
-        title: err.message,
-        duration:2000
-      })
+      // wx.showToast({
+      //   title: err.message,
+      //   duration:2000
+      // })
     });
   },
   //
@@ -53,48 +55,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
